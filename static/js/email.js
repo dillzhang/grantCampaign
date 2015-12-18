07db819c4979fccb354610c94957bacf34f1ad54
Nellie <nellie.spektor@gmail.com>
2015-12-17 21:08:35 -0500

    donate

--- static/js/email.js
+++ static/js/email.js
@@ -6,16 +6,24 @@ var buttonCallback = function(e) {
     for (i = 0; i < emailer.value.length; i++) {
 	emptyString += " ";
     }
-
+    
     if (emailer.value != emptyString) {
 	emailer.value = "";
 	checker.checked = false;
 	confirm("Thank you for joining our campaign! You'll be updated.");
     }
+    
+};
 
-}
-
-
+var buttonCallBack2 = function(e){
+    confirm("You're funny, now go donate to a real cause if you have so much extra money");
+};
 
 var button = document.getElementById("signUp");
 button.addEventListener("click",buttonCallback);
+
+var button2 = document.getElementById("donate");
+button2.addEventListener("click",buttonCallBack2);
+
+
+    
