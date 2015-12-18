6c32182c61594cfc3fdd1017fb2697997b590f06
Dillon Zhang <d1998z@gmail.com>
2015-12-17 19:43:10 -0500

    issues

---
+++ static/js/issues.js
@@ -0,0 +1,87 @@
+$( "#dt1" ).click(function() {
+    $( "#dd1" ).slideToggle( "slow", function() {
+    });
+});
+$( "#dt1" ).mouseover(function() {
+    $( "#dt1" ).css({ 'color': 'blue'});
+});
+$( "#dt1" ).mouseout(function() {
+    $( "#dt1" ).css({ 'color': 'black'});
+});
+
+$( "#dt2" ).click(function() {
+    $( "#dd2" ).slideToggle( "slow", function() {
+    });
+});
+$( "#dt2" ).mouseover(function() {
+    $( "#dt2" ).css({ 'color': 'blue'});
+});
+$( "#dt2" ).mouseout(function() {
+    $( "#dt2" ).css({ 'color': 'black'});
+});
+
+$( "#dt3" ).click(function() {
+    $( "#dd3" ).slideToggle( "slow", function() {
+    });
+});
+$( "#dt3" ).mouseover(function() {
+    $( "#dt3" ).css({ 'color': 'blue'});
+});
+$( "#dt3" ).mouseout(function() {
+    $( "#dt3" ).css({ 'color': 'black'});
+});
+
+$( "#dt4" ).click(function() {
+    $( "#dd4" ).slideToggle( "slow", function() {
+    });
+});
+$( "#dt4" ).mouseover(function() {
+    $( "#dt4" ).css({ 'color': 'blue'});
+});
+$( "#dt4" ).mouseout(function() {
+    $( "#dt4" ).css({ 'color': 'black'});
+});
+
+$( "#dt5" ).click(function() {
+    $( "#dd5" ).slideToggle( "slow", function() {
+    });
+});
+$( "#dt5" ).mouseover(function() {
+    $( "#dt5" ).css({ 'color': 'blue'});
+});
+$( "#dt5" ).mouseout(function() {
+    $( "#dt5" ).css({ 'color': 'black'});
+});
+
+$( "#ft3" ).click(function() {
+    $( "#fd3" ).slideToggle( "slow", function() {
+    });
+});
+$( "#ft3" ).mouseover(function() {
+    $( "#ft3" ).css({ 'color': 'blue'});
+});
+$( "#ft3" ).mouseout(function() {
+    $( "#ft3" ).css({ 'color': 'black'});
+});
+
+$( "#ft2" ).click(function() {
+    $( "#fd2" ).slideToggle( "slow", function() {
+    });
+});
+$( "#ft2" ).mouseover(function() {
+    $( "#ft2" ).css({ 'color': 'blue'});
+});
+$( "#ft2" ).mouseout(function() {
+    $( "#ft2" ).css({ 'color': 'black'});
+});
+
+$( "#ft1" ).click(function() {
+    $( "#fd1" ).slideToggle( "slow", function() {
+    });
+});
+$( "#ft1" ).mouseover(function() {
+    $( "#ft1" ).css({ 'color': 'blue'});
+});
+$( "#ft1" ).mouseout(function() {
+    $( "#ft1" ).css({ 'color': 'black'});
+});
